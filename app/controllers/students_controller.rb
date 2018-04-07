81819580863baace603f674013ba4c1c3475cdfa
Chely Ho <adept_inept@yahoo.com>
2018-04-07 00:57:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,11 +13,11 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-
+binding.pry
     @student.first_name = params[:first][:name]
     @student.last_name = params[:last][:name]
     @student.save
-      binding.pry
+
     redirect_to @student
   end
 
