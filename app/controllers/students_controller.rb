3d1e270121000786845ebdbbfd037ff3973bb1c0
Chely Ho <adept_inept@yahoo.com>
2018-04-07 00:37:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create()
   end
 
 end
